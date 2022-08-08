83b57fa9d1fd27542b8a8097125cba057cc1c63a
elai <matroskiin2014@gmail.com>
2022-08-08 20:32:46 +0300

    complete work with f lines. rename method interface to run. try use table to interface output

--- main.py
+++ main.py
@@ -42,62 +42,60 @@ class tamagocha():
             self.time = datetime.datetime.now()
 
 
-    def interface(self):
+    def run(self):
         do = int(input())
 
         if do == 1:
-            print(f'\nСтатус \n{str(self.name)} \nГолод: {str(self.eda)} \nУсталость: {str(self.tired)} \nУровень загрязненности комнаты: {str(self.trash)} \nПотебность в общении: {self.socialka} \nПотребность в страданиях: {self.bdsm}\n')
+            print(f'\nСтатус: \t{self.name} \nГолод: \t{self.eda} \nУсталость: \t{self.tired} \nУровень загрязненности комнаты: \t{self.trash} \nПотебность в общении: \t{self.socialka} \nПотребность в страданиях: \t{self.bdsm}\n')
 
         elif do == 2:
             self.eda = 100
-            print("Спасибо было вкусно!\n" + "Голод = " + str(self.eda))
+            print(f'\nСпасибо было вкусно! \nГолод: {self.eda}')
 
         elif do == 3:
             self.tired = 100
-            print("Я выспалась!\n" + "Усталость = " + str(self.tired))
+            print(f'\nЯ выспалась! \nУсталость: {self.tired}')
 
         elif do == 4:
             self.trash = 100
-            print("Какая чистота!\n" + "Уровень загрязнения = " + str(self.trash))
+            print(f'\nКакая чистота! \nУровень загрязнения: {self.trash}')
 
         elif do == 5:
             self.socialka = 100
-            print("Я так хорошо погуляла!\n" + "Потребность в общении = " + str(self.socialka))
+            print(f'\nЯ так хорошо погуляла! \nПотребность в общении: {self.socialka}')
 
         elif do == 6:
             self.bdsm = 100
-            print("Это было великолепно!\n" + "Потребность в страданиях = " + str(self.bdsm))
+            print(f'\nЭто было великолепно! \nПотребность в страданиях: {self.bdsm}')
 
         elif do == 7:
             return do
 
         else:
-            print("Я не понимаю, повторите еще раз.")
+            print(f"Я не понимаю, повторите еще раз.")
 
         if self.eda <= 0:
-            print("Ваш персонаж умер от голода")
+            print(f"Ваш персонаж умер от голода")
         elif self.trash <= 0:
-            print("Ваш персонаж умер в грязи от инфекций")
+            print(f"Ваш персонаж умер в грязи от инфекций")
         elif self.bdsm <= 0:
-            print("Ваш персонаж совершил суицид")
+            print(f"Ваш персонаж совершил суицид")
         elif self.tired <= 0:
-            print("Ваш персонаж устал до смерти")
+            print(f"Ваш персонаж устал до смерти")
         elif self.socialka <= 0:
-            print("Ваш персонаж умер от одиночества")
+            print(f"Ваш персонаж умер от одиночества")
         return do
 
 
 def main():
 
-    print("Добро пожаловать домой! Что вы хотите сделать, хозяин?\n" + "Посмотреть статус - 1\n"
-        + "Покормить - 2\n" + "Отправить в кровать - 3\n"
-        + "Навести порядки и покупать - 4\n" + "Отправить на прогулку - 5\n" + "Отправить в БДСМ клуб - 6\n")
+    print(f"Добро пожаловать домой! Что вы хотите сделать, хозяин?\n Посмотреть статус - 1\n Покормить - 2\n Отправить в кровать - 3\n Навести порядки и покупать - 4\n Отправить на прогулку - 5\n Отправить в БДСМ клуб - 6\n")
 
     tamagocha1 = tamagocha("data.json")
-
-    now = datetime.datetime.today()
-
+    
     """
+    now = datetime.datetime.today()
+    
     time1 = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
     c = str(now - time1)
     diff = datetime.datetime.strptime(c, "%H:%M:%S.%f")
@@ -111,7 +109,7 @@ def main():
     """
     end=0
     while end!=7:
-        end=tamagocha1.interface()
+        end=tamagocha1.run()
 
 
 if __name__ == "__main__":
