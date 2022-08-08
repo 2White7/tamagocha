fdb3f3bad53c5bf6b967825d090fbb98004b79fc
elai <matroskiin2014@gmail.com>
2022-08-08 20:18:49 +0300

    refactor __init__ and r/w func. temporary comment time functionality. start work with f lines

--- main.py
+++ main.py
@@ -2,26 +2,51 @@ import json
 import datetime
 
 
+def read_data(name):
+    with open(name, "r") as f:
+            return json.load(f)
+
+def write_data(name):
+    tamplate = dict()
+
+    tamplate["name"] = tamagocha1.name
+    tamplate["eda"] = tamagocha1.eda
+    tamplate["socialka"] = tamagocha1.socialka
+    tamplate["bdsm"] = tamagocha1.bdsm
+    tamplate["trash"] = tamagocha1.trash
+    tamplate["tired"] = tamagocha1.tired
+
+    with open(name, "w") as f:
+        json.dump(tamplate, f)
+
 class tamagocha():
-    def __init__(self, data, name, eda = 100, socialka = 100, bdsm = 100, trash = 100, tired = 100):        
-        self.name = name
-        self.eda = eda
-        self.socialka = socialka
-        self.bdsm = bdsm
-        self.trash = trash
-        self.tired = tired
+    def __init__(self, data):        
+        try:
+            temp = read_data(data)
+
+            self.name = temp["name"]
+            self.eda = temp["eda"]
+            self.socialka = temp["socialka"]
+            self.bdsm = temp["bdsm"]
+            self.trash = temp["trash"]
+            self.tired = temp["tired"]
+            self.time = temp["time"]
+        
+        except:
+            self.name = "Princess"
+            self.eda = 100
+            self.socialka = 100
+            self.bdsm = 100
+            self.trash = 100
+            self.tired = 100
+            self.time = datetime.datetime.now()
 
 
     def interface(self):
         do = int(input())
 
         if do == 1:
-            print("Статус\n" + str(self.name) +
-                ("\nГолод = " + str(self.eda)) +
-                ("\nУсталость = " + str(self.tired)) +
-                ("\nУровень загрязненности комнаты = " + str(self.trash)) +
-                ("\nПотебность в общении = "+ str(self.socialka)) +
-                ("\nПотребность в страданиях = " + str(self.bdsm)))
+            print(f'\nСтатус \n{str(self.name)} \nГолод: {str(self.eda)} \nУсталость: {str(self.tired)} \nУровень загрязненности комнаты: {str(self.trash)} \nПотебность в общении: {self.socialka} \nПотребность в страданиях: {self.bdsm}\n')
 
         elif do == 2:
             self.eda = 100
@@ -63,44 +88,31 @@ class tamagocha():
 
 
 def main():
-    with open("data.json", "r") as f:
-        tamplate = json.load(f)
 
     print("Добро пожаловать домой! Что вы хотите сделать, хозяин?\n" + "Посмотреть статус - 1\n"
         + "Покормить - 2\n" + "Отправить в кровать - 3\n"
         + "Навести порядки и покупать - 4\n" + "Отправить на прогулку - 5\n" + "Отправить в БДСМ клуб - 6\n")
 
-    tamagocha1 = tamagocha(tamplate["name"], tamplate["eda"],
-                        tamplate["socialka"], tamplate["bdsm"],
-                        tamplate["trash"],tamplate["tired"])
+    tamagocha1 = tamagocha("data.json")
 
     now = datetime.datetime.today()
 
-    time = tamplate["time"]
-    tamplate["time"] = str(now)
-
+    """
     time1 = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
     c = str(now - time1)
     diff = datetime.datetime.strptime(c, "%H:%M:%S.%f")
+
     if diff.hour > 0:
         tamagocha1.eda = tamagocha1.eda - (10 * diff.hour)
         tamagocha1.bdsm = tamagocha1.bdsm - (10 * diff.hour)
         tamagocha1.trash = tamagocha1.trash - (10 * diff.hour)
         tamagocha1.tired = tamagocha1.tired - (10 * diff.hour)
         tamagocha1.socialka = tamagocha1.socialka - (10 * diff.hour)
-
+    """
     end=0
     while end!=7:
         end=tamagocha1.interface()
 
-    tamplate["eda"] = tamagocha1.eda
-    tamplate["socialka"] = tamagocha1.socialka
-    tamplate["bdsm"] = tamagocha1.bdsm
-    tamplate["trash"] = tamagocha1.trash
-    tamplate["tired"] = tamagocha1.tired
-
-    with open("data.json", "w") as f:
-        json.dump(tamplate, f)
 
 if __name__ == "__main__":
     main()
